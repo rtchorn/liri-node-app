5e17fde77add3cb2c575435381564abe0582b459
rtchorn <riphydiachorn@gmail.com>
2017-05-28 23:15:14 -0700

    final

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'jcWBpnC5tUDtTs76Pg3X1S30R',
+  consumer_secret: 'RGaWk5OS5BJepjqPzWVZX8n3L8u9ct5XXQAavJ2DZikDFJx7o0',
+  access_token_key: '869070919192281088-u9SlqkF5d5Q2LjmQaMWHMULancb8sev',
+  access_token_secret: 'ycUuJGPH0QBz0BHtMTPW2T2rJ212FlbnVPeDT7rxC6A9C',
+}
